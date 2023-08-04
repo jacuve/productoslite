efea8f7aa5b5fd99fd451dd8a180b8bf61e6154a
Jose C <jacuve@gmail.com>
2023-08-04 23:59:33 +0200

    Initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src=
+"https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js">
+    </script>
+</head>
+<body  class="my-4">
+    
+    @yield('contenido')
+</body>
+</html>
